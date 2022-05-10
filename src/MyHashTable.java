8c17bd7b5cf581b6ff0c3d95e2cc1ab1668e2f78
AST-ПК <amirsilver@mail.ru>
2022-05-10 22:15:20 +0600

    fourth

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -98,3 +98,118 @@ public class MyHashTable<K,V> {
     }
 
 }
+
+public class BST<K extends Comparable <K>, V>{
+    private Node root;
+    private class Node{
+        private K key;
+        private V val;
+        private int N;
+        public Node left, right;
+        public Node(K key, V val){
+            this.key=key;
+            this.val=val;
+            this.N=N;
+        }
+    }
+    public int size() {
+        return size(root);
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        else return x.N;
+    }
+
+    public boolean contains(K key) {
+        return get(key) != null;
+    }
+
+
+    public V get(K key) {
+        return get(root, key);
+    }
+
+    private V get(Node x, K key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else              return x.val;
+    }
+
+    public void put(K key, V val) {
+        if (val == null){
+            delete(key);
+            return;
+        }
+        root = put(root, key, val);
+
+    }
+
+    private Node put(Node x, K key, V val) {
+        if (x == null) return new Node(key, val, 1);
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) x.left  = put(x.left,  key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else              x.val   = val;
+        x.N = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) throw new RuntimeException("Symbol table underflow");
+        root = deleteMin(root);
+
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void deleteMax() {
+        if (isEmpty()) throw new RuntimeException("Symbol table underflow");
+        root = deleteMax(root);
+
+    }
+
+    private Node deleteMax(Node x) {
+        if (x.right == null) return x.left;
+        x.right = deleteMax(x.right);
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void delete(K key) {
+        root = delete(root, key);
+
+    }
+    public K min() {
+        if (isEmpty()) return null;
+        return min(root).key;
+    }
+
+    private Node delete(Node x, K key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) x.left  = delete(x.left,  key);
+        else if (cmp > 0) x.right = delete(x.right, key);
+        else {
+            if (x.right == null) return x.left;
+            if (x.left  == null) return x.right;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.N = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+}
